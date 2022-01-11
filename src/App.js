5156ffa1f08b40da1d9ae7a47363ef37a8118719
khasheshaikh <khasheshaikh@gmail.com>
2022-01-11 12:51:14 +0530

    .

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './App.css';
 import Weather from './components/Weather'
 
 function App() {
-  async function GetWeatherData ({lat,lon}) {
+  async function GetWeatherData ({city}) {
       
       
     try {
       const response = await fetch(
-       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=104df50695a2f73b2d604242576c5690`
+       `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=104df50695a2f73b2d604242576c5690`
+       
       );
       const data = await response.json();
       if(data){
@@ -34,7 +35,7 @@ function App() {
   
   return (
     <div>
-    <h1></h1>
+    <h1>Welcome</h1>
       <Weather onFetchWeather={GetWeatherData}/>
     </div>
   
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,37 +1,38 @@
 import React,{useState} from 'react';
-import {Row,Col,Button} from "react-bootstrap";
+import {Row,Col,Button,DropdownButton,Dropdown} from "react-bootstrap";
 import "./Weather.css";
 
 
 export default function Weather ({onFetchWeather}) {
   
     const [data, setData] = useState([]);
-    const [lat, setLat] = useState("19.07");
-    const [lon, setLon] = useState("72.87");
+    const [city, setCity] = useState("mumbai");
+    
     
 
     async function GetWeatherData() {
-      const data = await onFetchWeather({lat,lon});
-  
+      const data = await onFetchWeather({city});
+         
   if (data) {
     setData(data);
     console.log(data);
+    
+    
   }else{
     console.log("fetch error");
   }
 
 }
-function handleLatChange(event) {
-  setLat(event.target.value);
+function handleCityChange(event) {
+  setCity(event.target.value);
 }
 
-function handleLanChange(event) {
-  setLon(event.target.value);
-}
+
     return(
+      
       <div>
       
-       
+      
 <div className="center">
   <h1>5- day forecast</h1>
   
@@ -39,65 +40,69 @@ function handleLanChange(event) {
       
 
 <div className="center">
-<input value={lat} onChange={handleLatChange} type="number"></input>
-<input value={lon} onChange={handleLanChange} type="number"></input>
+<input value={city} onChange={handleCityChange} type="text"></input>
+{/* <DropdownButton id="dropdown-basic-button" title={city}>
+  <Dropdown.Item href="#/action-1" onClick={(e) => setCity(e.target.title)}>mumbai</Dropdown.Item>
+  <Dropdown.Item href="#/action-2" onClick={(e) => setCity(e.target.title)}>nanded</Dropdown.Item>
+  <Dropdown.Item href="#/action-3" onClick={(e) => setCity(e.target.title)}>pune</Dropdown.Item>
+</DropdownButton> */}
+
 <Button onClick={GetWeatherData}>fetch</Button>
+<h1></h1>
 </div>
 
 
-
- <Row>
+    <Row>
 <Col md="1"></Col>
 <Col md="2">
 <div className="card">
       <h2>Today</h2>
-      <h5>{data.timezone}</h5>
-      <h1>{data.daily[0].temp.day}</h1>
-      <h2>{data.daily[0].weather[0].main}</h2>
-      <img src={`http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png` } alt="hi"></img>
+      <h5>{data.list[0].dt_txt}</h5>
+      <h1>{data.list[0].main.temp}°C</h1>
+      <h2>{data.list[0].weather[0].main}</h2>
+      <img src={`http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png` } alt="hi"></img>
+      
 </div>  
 </Col>
 <Col md="2">
 <div className="card">
 <h2>Yesterday</h2>
-      <h5>{data.timezone}</h5>
-      <h1>{data.daily[1].temp.day}</h1>
-      <h2>{data.daily[1].weather[0].main}</h2>
-      <img src={`http://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}@2x.png`} alt="hi"></img>
+      <h5>{data.list[8].dt_txt}</h5>
+      <h1>{data.list[8].main.temp}°C</h1>
+      <h2>{data.list[8].weather[0].main}</h2>
+      <img src={`http://openweathermap.org/img/wn/${data.list[8].weather[0].icon}@2x.png` } alt="hi"></img>
 </div>  
 </Col>
 <Col md="2">
 <div className="card">
 <h2>2 days ago</h2>
-      <h5>{data.timezone}</h5>
-      <h1>{data.daily[2].temp.day}</h1>
-      <h2>{data.daily[2].weather[0].main}</h2>
-      <img src={`http://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}@2x.png`} alt="hi"></img>
+      <h5>{data.list[16].dt_txt}</h5>
+      <h1>{data.list[16].main.temp}°C</h1>
+      <h2>{data.list[16].weather[0].main}</h2>
+      <img src={`http://openweathermap.org/img/wn/${data.list[16].weather[0].icon}@2x.png` } alt="hi"></img>
 </div>  
 </Col>
 <Col md="2">
 <div className="card">
 <h2>3 days ago</h2>
-      <h5>{data.timezone}</h5>
-      <h1>{data.daily[3].temp.day}</h1>
-      <h2>{data.daily[3].weather[0].main}</h2>
-      <img src={`http://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}@2x.png`} alt="hi"></img>
+      <h5>{data.list[24].dt_txt}</h5>
+      <h1>{data.list[24].main.temp}°C</h1>
+      <h2>{data.list[24].weather[0].main}</h2>
+      <img src={`http://openweathermap.org/img/wn/${data.list[24].weather[0].icon}@2x.png` } alt="hi"></img>
 </div>  
 </Col>
 <Col md="2">
 <div className="card">
 <h2>4 days</h2>
-      <h5>{data.timezone}</h5>
-      <h1>{data.daily[4].temp.day}</h1>
-      <h2>{data.daily[4].weather[0].main}</h2>
-      <img src={`http://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}@2x.png`} alt="hi"></img>
+      <h5>{data.list[32].dt_txt}</h5>
+      <h1>{data.list[32].main.temp}°C</h1>
+      <h2>{data.list[32].weather[0].main}</h2>
+      <img src={`http://openweathermap.org/img/wn/${data.list[32].weather[0].icon}@2x.png` } alt="hi"></img>
 </div>  
 </Col>
 <Col md="1"></Col>
 
 </Row> 
-
-     
 </div>
   )
     }
